97ac2b935b0daa4c8ed8b6b3da6c33920be8c629
marklin2289 <marklin2289@gmail.com>
2022-08-05 23:57:56 -0700

    created contract IWETH

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,30 +11,30 @@ require("hardhat-deploy")
  */
 
 const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL
-const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL || ""
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
-const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL
+// const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || process.env.ALCHEMY_MAINNET_RPC_URL
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+// const KOVAN_RPC_URL = process.env.KOVAN_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
             chainId: 31337,
-            forking: {
-                url: MAINNET_RPC_URL,
-            },
+            // forking: {
+            //     url: MAINNET_RPC_URL,
+            // },
         },
         localhost: {
             chainId: 31337,
         },
-        kovan: {
-            url: KOVAN_RPC_URL,
-            accounts: [PRIVATE_KEY],
-            chainId: 42,
-            blockConfirmations: 6,
-        },
+        // kovan: {
+        //     url: KOVAN_RPC_URL,
+        //     accounts: [PRIVATE_KEY],
+        //     chainId: 42,
+        //     blockConfirmations: 6,
+        // },
         rinkeby: {
             url: RINKEBY_RPC_URL,
             accounts: [PRIVATE_KEY],
--- scripts/getWeth.js
+++ scripts/getWeth.js
@@ -1,1 +1,9 @@
- 
+const { getNamedAccounts } = require("hardhat")
+
+async function getWeth() {
+    const { deployer } = await getNamedAccounts()
+    // call the "deposit" function on the weth contract :
+    // need : abi, contract address
+}
+
+module.exports = { getWeth }
