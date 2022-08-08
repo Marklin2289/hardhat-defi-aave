35e9b291a257f30003b83195e425586012257a32
marklin2289 <marklin2289@gmail.com>
2022-08-07 23:39:51 -0700

    edited aaveBorrow.js

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,8 +1,14 @@
-const { getWeth } = require("../scripts/getWeth")
+const { getNamedAccounts } = require("hardhat")
+const { getWeth } = require("./getWeth")
 
 async function main() {
     // the protocol treats everything as an ERC20 token
     await getWeth()
+    const { deployer } = await getNamedAccounts()
+    // abi, address
+
+    // Lending Pool Address Provider : 0xB53C1a33016B2DC2fF3653530bfF1848a515c8c5
+    // Lending Pool : ^
 }
 
 main()
